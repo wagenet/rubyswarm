5754ea0d420668419b1dbeb00f4e57bebd8a237c
Peter Wagenet <peter.wagenet@gmail.com>
2010-12-17 16:34:06 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubySwarm::Application.config.secret_token = 'e7c8ec38a0d92287ec9e96e3fcb1a0bce6654b4c66e9a6bfc2dc62b92a36bdfeedf6c02163fca7331a5728dee584de99720b315acd1100ef858293926355908f'
