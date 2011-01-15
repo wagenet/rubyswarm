30a47337a8b4a803f89e48da4731d5071f58746a
Peter Wagenet <peter.wagenet@gmail.com>
2011-01-14 17:41:56 -0800

    Added Strobe Proxy

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+RubySwarm::Application.configure do
+  config.middleware.use Strobe::Middleware::Proxy
+end
+
 # Initialize the rails application
 RubySwarm::Application.initialize!
