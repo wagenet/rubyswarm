00269d05efb7c4e2689394b998b2720cbf6eaa21
Peter Wagenet <peter.wagenet@gmail.com>
2011-01-19 13:33:56 -0800

    Better test results display

--- app/controllers/client_runs_controller.rb
+++ app/controllers/client_runs_controller.rb
@@ -2,9 +2,16 @@ class ClientRunsController < ApplicationController
   load_and_authorize_resource
 
   def show
+=begin
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @client_runs }
+    end
+=end
+    if @client_run.results
+      send_data @client_run.results, :type => 'text/html', :disposition => 'inline'
+    else
+      render :text => 'No Saved Results', :layout => false
     end
   end
 
