45fc1a5043f969a5f862a39e6309c617dcaa7182
Peter Wagenet <peter.wagenet@gmail.com>
2011-01-18 22:04:44 -0800

    Expanded require statement

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require 'github_proxy'
+require File.join(Rails.root, 'lib', 'github_proxy')
 
 RubySwarm::Application.configure do
   config.middleware.use GithubProxy
