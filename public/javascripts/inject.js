af3005e948e8d9e5ecb6bfb77c8179171e78983f
Peter Wagenet <peter.wagenet@gmail.com>
2011-01-19 11:17:43 -0800

    Save test results

--- public/javascripts/inject.js
+++ public/javascripts/inject.js
@@ -14,7 +14,7 @@
       curKeepalive = setTimeout(keepalive, keepaliveRate * 1000);
     },
     serialize: function(){
-      console.log('serialize');
+      return trimSerialize();
     }
   };
 
@@ -43,39 +43,51 @@
       return ret;
     };
 
+    window.RubySwarmRunner.serialize = function(){
+      console.log('CoreTest serialize');
+      var hidePassed = document.getElementsByClassName('hide-passed');
+      while ( hidePassed.length ) { remove( hidePassed[0] ); }
+
+      var clean = document.getElementsByClassName('clean');
+      while ( clean.length ) { remove( clean[0] ); }
+
+      return trimSerialize();
+    };
+
   // QUnit (jQuery)
-	// http://docs.jquery.com/QUnit
+  // http://docs.jquery.com/QUnit
   } else if ( typeof QUnit !== "undefined" ) {
-		QUnit.done = function(fail, total){
-			submitSuccess({
-				fail: fail,
-				error: 0,
-				total: total
-			});
-		};
+    QUnit.done = function(fail, total){
+      submitSuccess({
+        fail: fail,
+        error: 0,
+        total: total
+      });
+    };
 
-		QUnit.log = window.RubySwarmRunner.keepalive;
-		window.RubySwarmRunner.keepalive();
+    QUnit.log = window.RubySwarmRunner.keepalive;
+    window.RubySwarmRunner.keepalive();
 
-		window.RubySwarmRunner.serialize = function(){
-			// Clean up the HTML (remove any un-needed test markup)
-			remove("nothiddendiv");
-			remove("loadediframe");
-			remove("dl");
-			remove("main");
+    window.RubySwarmRunner.serialize = function(){
+      // Clean up the HTML (remove any un-needed test markup)
+      remove("nothiddendiv");
+      remove("loadediframe");
+      remove("dl");
+      remove("main");
 
-			// Show any collapsed results
-			var ol = document.getElementsByTagName("ol");
-			for ( var i = 0; i < ol.length; i++ ) {
-				ol[i].style.display = "block";
-			}
+      // Show any collapsed results
+      var ol = document.getElementsByTagName("ol");
+      for ( var i = 0; i < ol.length; i++ ) {
+        ol[i].style.display = "block";
+      }
 
-			return trimSerialize();
-		};
+      return trimSerialize();
+    };
   }
 
   function submit(params){
     if (window.top.RubySwarm && window.top.RubySwarm.submitTest) {
+      params.results = window.RubySwarmRunner.serialize();
       window.top.RubySwarm.submitTest(params);
     } else {
       console.log('submit', params);
@@ -100,6 +112,43 @@
       window.top.RubySwarm.keepalive();
     } else {
       console.log('keepalive');
+    }
+  }
+
+  function trimSerialize(doc) {
+    console.log('trimSerialize');
+    doc = doc || document;
+
+    var scripts = doc.getElementsByTagName("script");
+    while ( scripts.length ) {
+      remove( scripts[0] );
+    }
+
+    var root = window.location.href.replace(/(https?:\/\/.*?)\/.*/, "$1");
+    var cur = window.location.href.replace(/[^\/]*$/, "");
+
+    var links = doc.getElementsByTagName("link");
+    for ( var i = 0; i < links.length; i++ ) {
+      var href = links[i].href;
+      if ( href.indexOf("/") === 0 ) {
+        href = root + href;
+      } else if ( !/^https?:\/\//.test( href ) ) {
+        href = cur + href;
+      }
+      links[i].href = href;
+    }
+
+    return ("<html>" + doc.documentElement.innerHTML + "</html>")
+      .replace(/\s+/g, " ");
+  }
+
+  function remove(elem){
+    if ( typeof elem === "string" ) {
+      elem = document.getElementById( elem );
+    }
+
+    if ( elem ) {
+      elem.parentNode.removeChild( elem );
     }
   }
 
