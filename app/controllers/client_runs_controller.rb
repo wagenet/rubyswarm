4d758b706ba6bd6d421b7c3fd6972497aedca2ae
Peter Wagenet <peter.wagenet@gmail.com>
2011-01-21 00:35:31 -0800

    Remove old code

--- app/controllers/client_runs_controller.rb
+++ app/controllers/client_runs_controller.rb
@@ -2,12 +2,6 @@ class ClientRunsController < ApplicationController
   load_and_authorize_resource
 
   def show
-=begin
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @client_runs }
-    end
-=end
     if @client_run.results
       send_data @client_run.results, :type => 'text/html', :disposition => 'inline'
     else
